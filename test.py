a5d4bfd4a6744d8e130d53d498362f897f6c18a3
Doan Duc Minh <114096378+minh2004pd@users.noreply.github.com>
2024-04-26 21:36:49 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,106 @@
+import requests
+import uuid
+import hashlib
+import pydicom
+from pathlib import Path
+from pydicom.filebase import DicomBytesIO
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import requests_toolbelt as tb
+from io import BytesIO
+import matplotlib.pyplot as plt
+import os
+
+path_to_dicoms_dir = Path('D:\OneDrive\Máy tính\python_dicom\dicom_files')
+
+base_url = 'http://localhost:8042'
+
+username = 'orthanc'
+password = 'orthanc'
+
+client = requests.session()
+
+
+# Create the URL for the instance
+url = f'{base_url}/studies'
+
+headers = {'Accept':'multipart/related; type="application/octet-stream"; transfer-syntax=*'}
+
+response = client.get(url, headers=headers, auth=(username, password))
+
+# Define the base URL of the Orthanc server
+base_url = 'http://localhost:8042'
+
+# Send a GET request to the Orthanc server to get a list of all studies
+response = requests.get(url, auth=(username, password))
+
+def get_series(study):
+    response = requests.get(f'{base_url}/studies/{study}/series', auth=(username, password))
+    return response.json()
+
+def get_instances(series):
+    response = requests.get(f'{base_url}/series/{series["ID"]}/instances', auth=(username, password))
+    return response.json()
+
+def get_dicom_file_data(instance):
+    response = requests.get(f'{base_url}/instances/{instance["ID"]}/file', auth=(username, password))
+    if response.status_code == 200:
+        return response.content
+    else:
+        return None
+
+def get_study_name(study):
+    response = requests.get(f'{base_url}/studies/{study}', auth=(username, password))
+    # print("Status code:", response.status_code)
+    # print("Response data:", response.json())
+    if response.status_code == 200:
+        return response.json().get('PatientMainDicomTags').get('PatientName')
+    else:
+        return None
+
+def write_dicom_file_data(dicom_file_data, name, cnt):
+    directory = f'dicom_files_import/{name}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(os.path.join(directory, f'{cnt}.dcm'), 'wb') as file:
+        file.write(dicom_file_data)
+
+def get_all_studies(studies):
+    for study in studies:
+        series_list = get_series(study)
+        name = get_study_name(study)
+        for series in series_list:
+            instances = get_instances(series)
+            cnt = 0
+            for instance in instances:
+                dicom_file_data = get_dicom_file_data(instance)
+                if dicom_file_data is not None:
+                    write_dicom_file_data(dicom_file_data, name, cnt)
+                    cnt += 1
+
+def get_studies_by_name(studies, name):
+    for study in studies:
+        study_name = get_study_name(study)
+        if study_name == name:
+            series_list = get_series(study)
+            for series in series_list:
+                instances = get_instances(series)
+                cnt = 0
+                for instance in instances:
+                    print(instance['MainDicomTags']['InstanceNumber'])
+                    break
+                    # dicom_file_data = get_dicom_file_data(instance)
+                    # if dicom_file_data is not None:
+                    #     write_dicom_file_data(dicom_file_data, name, cnt)
+                    #     cnt += 1
+
+# Check if the request was successful
+if response.status_code == 200:
+    # The request was successful
+    # The response body contains a list of all studies
+    studies = response.json()
+    get_studies_by_name(studies, 'TCGA-CS-5396')
+
+    print("DICOM files retrieved successfully!")
+else:
+    # The request failed
+    print("Failed to retrieve DICOM files. Status code:", response.status_code)
---
+++ upload_dicom.py
@@ -0,0 +1,46 @@
+import requests
+import pydicom
+from pathlib import Path
+from pydicom.filebase import DicomBytesIO
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+path_to_dicoms_dir = Path('D:\OneDrive\Máy tính\python_dicom\dicom_files')
+
+base_url = 'http://localhost:8042'
+
+client = requests.session()
+
+url= f'{base_url}/instances'
+
+response = client.get(url, auth=('orthanc', 'orthanc'))
+if (response.status_code != 200):
+    print('Error! Likely not authenticated!')
+else: 
+    print('authenticated!')
+
+# Loop over all DICOM files in the directory
+for filepath in path_to_dicoms_dir.glob('*.dcm'):
+    # Read the DICOM file with pydicom
+    dataset = pydicom.dcmread(filepath)
+
+    # Convert the DICOM dataset to bytes
+    with DicomBytesIO() as f:
+        dataset.save_as(f, write_like_original=True)
+        rawfile = f.parent.getvalue()
+
+    # Create a MultipartEncoder object
+    multipart_data = MultipartEncoder(
+        fields={
+            'file': ('dicomfile', rawfile, 'application/dicom')
+        }
+    )
+
+    headers = {'Accept':'application/dicom+json', "Content-Type": multipart_data.content_type}
+
+    url = f'{base_url}/instances'
+    response = requests.post(url, data=multipart_data, headers=headers, verify=False, auth=('orthanc', 'orthanc'))
+    if response.status_code != 200:
+        print(f'Error uploading {filepath}! Status code:', response.status_code)
+        print('Response:', response.text)
+    else:
+        print(f'Successfully uploaded {filepath}!')
